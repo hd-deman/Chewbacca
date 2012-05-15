39afdd1579314019672e988a19f91531fd0076e9
Dmitry Tsoy <hd.deman@gmail.com>
2012-05-15 17:10:23 +0400

    breadcrumbs navigation

---
+++ CoreBundle/Controller/BreadcrumbsController.php
@@ -0,0 +1,16 @@
+<?php
+namespace Chewbacca\CoreBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller,
+Doctrine\ORM\Query,
+Chewbacca\CoreBundle\Entity\Category;
+
+class BreadcrumbsController extends Controller
+{
+	public function pathAction($breadcrumbs = array())
+	{
+		$breadcrumbs = array_merge(array(array('title' => 'главная', 'url' => $this->generateUrl('homepage'))), $breadcrumbs);
+
+		return $this->render('ChewbaccaCoreBundle:Breadcrumbs:path.html.twig', array('breadcrumbs' => $breadcrumbs));
+	}
+}
