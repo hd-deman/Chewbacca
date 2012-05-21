f093b37bb45dfd3c78478639ecc8ed2cf6920dfb
Dmitry Tsoy <hd.deman@gmail.com>
2012-05-21 14:41:32 +0400

    fix mails

--- UserBundle/Mailer/Mailer.php
+++ UserBundle/Mailer/Mailer.php
@@ -8,8 +8,8 @@ class Mailer extends BaseMailer
 {
     public function sendNewUserEmailMessage(UserInterface $user, $password)
     {
-        $message = sprintf("new lacroco user\n username: %s email: %s pass: %s", $user->getUsername(), $user->getEmail(), $password);
-        $this->sendEmailMessage($message, $user->getEmail(), 'hd.deman@gmail.com');
-        $this->sendEmailMessage($message, $user->getEmail(), 'chief@lacroco.ru');
+        $message = sprintf("new lacroco user registered\n username: %s email: %s pass: %s", $user->getUsername(), $user->getEmail(), $password);
+        $this->sendEmailMessage($message, $this->parameters['from_email'], 'hd.deman@gmail.com');
+        $this->sendEmailMessage($message, $this->parameters['from_email'], 'chief@lacroco.ru');
     }
 }
