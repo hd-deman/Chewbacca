ac31f73bc4492a9e03bfcb7f73628185204a514f
Dmitry Tsoy <hd.deman@gmail.com>
2012-06-17 16:46:12 +0400

    fix artefacts

--- CoreBundle/DependencyInjection/ChewbaccaCoreExtension.php
+++ CoreBundle/DependencyInjection/ChewbaccaCoreExtension.php
@@ -2,23 +2,27 @@
 
 namespace Chewbacca\CoreBundle\DependencyInjection;
 
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
 
+/**
+ * This is the class that loads and manages your bundle configuration
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
+ */
 class ChewbaccaCoreExtension extends Extension
 {
+    /**
+     * {@inheritDoc}
+     */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $definition = new Definition('Chewbacca\CoreBundle\Twig\CoreExtension');
-        // this is the most important part. Later in the startup process TwigBundle
-        // searches through the container and registres all services taged as twig.extension.
-        $definition->addTag('twig.extension');
-        $container->setDefinition('core_extension', $definition);
-    }
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
 
-    public function getAlias()
-    {
-        return 'chewbacca_core';
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
