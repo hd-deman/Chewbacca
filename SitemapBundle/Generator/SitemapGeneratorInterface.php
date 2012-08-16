2aa20020952e040eea691a5d7f67f41ea9fc8990
Dmitry Tsoy <hd.deman@gmail.com>
2012-08-16 13:05:11 +0400

    init sitemap generator

---
+++ SitemapBundle/Generator/SitemapGeneratorInterface.php
@@ -0,0 +1,11 @@
+<?php
+namespace Chewbacca\SitemapBundle\Generator;
+
+/**
+ * Interface for sitemaps generator.
+ *
+ */
+interface SitemapGeneratorInterface
+{
+    public function generate();
+}
