af9badd03c612ffeaf86dd175798ff92a5d1d374
Dmitry Tsoy <hd.deman@gmail.com>
2012-08-29 15:10:21 +0400

    u r welcone mail

--- UserBundle/Mailer/Mailer.php
+++ UserBundle/Mailer/Mailer.php
@@ -11,4 +11,12 @@ class Mailer extends BaseMailer
         $message = sprintf("new lacroco user registered\n username: %s email: %s pass: %s", $user->getUsername(), $user->getEmail(), $password);
         $this->sendEmailMessage($message, array('admin@lacroco.ru' => 'Lacroco Admin'), 'chief@lacroco.ru');
     }
+
+    public function sendWelcomeMessage(UserInterface $user)
+    {
+        $rendered = $this->templating->render($template, array(
+            'user' => $user,
+        ));
+        $this->sendEmailMessage($message, array('admin@lacroco.ru' => 'Lacroco Admin'), $user->getEmail());
+    }
 }
